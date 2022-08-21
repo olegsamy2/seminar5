0dc752d6e898d7aa6877dadfe4e6429b9cff8bd4
OlegSam2 <doorsmania@mail.ru>
2022-08-21 12:40:44 +0300

    array change positive negative

---
+++ task32/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+ int[] array = new int[4];
+
+ Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+ {
+   array[i] = rand.Next( -9,10);
+  
+     array[i] = array[i] * 1;
+ }
+ Console.WriteLine(String.Join(",", array));
+ for (int i = 0; i < array.Length; i++)
+
+ {
+  
+     array[i] = array[i] * -1;
+     // array[i] = -array[i]
+ }
+
+ Console.WriteLine(String.Join(",", array));
+
+
+
